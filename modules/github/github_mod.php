964f29c98c6ebea86c3caeb02ae95218f4585859
g2x3k <faceydk@gmail.com>
2015-07-21 04:00:37 +0200

    New MOD: github_mod for listing and displaying issues on projects

---
+++ modules/github/github_mod.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: g2x3k
+ * Date: 21/7/2015
+ * Time: 03:10
+ */
+class github_mod extends module
+{
+
+    public $title = "github.issue mod";
+    public $author = "g2x3k";
+    public $version = "0.1";
+    private $delay = 0;
+
+    /*
+     * Todo:
+     * Watch for new issues ? (yes github have service but this more fun)
+     */
+
+
+    public function init()
+    {
+        // define what github repo to watch
+        $this->ghcfg["repo"] = "SimpleMachines/SMF2.1";
+    }
+
+    public function priv_showIssue($line, $args)
+    {
+        $channel = strtolower($line['to']);
+        $nick = $line['fromNick'];
+        // !issue [issue_num] [repo]
+        $issuenum = ($args["nargs"] > 0 ? $args['arg1'] : null);
+        $repo = ($args["nargs"] > 1 ? $args['arg2'] : $this->ghcfg["repo"]);
+
+
+        if ($issuenum) {
+            // list issue
+            $url = 'https://api.github.com/repos/' . str_replace('%2F', '/', urlencode($repo)) . '/issues/' . $issuenum;
+
+            $data = get_url_contents($url);
+            $data = json_decode($data["html"]);
+
+            if (empty($data)) {
+                //$this->bot->say('Fetching issue failed');
+                $this->ircClass->privMsg($channel, "Fetching failed, tried getting issue $issuenum from $repo");
+                return;
+            }
+
+            $this->ircClass->privMsg($channel , (isset($data->pull_request) ? 'Pull request' : 'Issue') . ' #' . $data->number . ': ' . $data->title . ' (' . $data->comments . ' comment(s))');
+            $this->ircClass->privMsg($channel , 'Reported by ' . $data->user->login . ', current status: ' . $data->state);
+            $this->ircClass->privMsg($channel , substr(preg_replace('/\s\s+/', ' ', $data->body), 0, 250));
+            $this->ircClass->privMsg($channel , 'More information: ' . $data->html_url);
+        }
+        else {
+            $this->ircClass->privMsg($channel , "Sending last 5 issue for $repo in privmsg");
+            $url = 'https://api.github.com/repos/' . str_replace('%2F', '/', urlencode($repo)) . '/issues';
+
+            $data = get_url_contents($url);
+            $result = json_decode($data["html"]);
+
+            $i = 0;
+            foreach ($result as $issue) {
+                if ($i >= 5)
+                    break;
+
+                $this->ircClass->privMsg($channel , (isset($issue->pull_request) ? '[Pull]' : '[Issue]') . ' #' . $issue->number . ': ' . $issue->title . ' (reported by ' . $issue->user->login . ', status: ' . $issue->state . ') <' . $issue->html_url . '>');
+
+                $i++;
+            }
+        }
+    }
+}
+
+?>
